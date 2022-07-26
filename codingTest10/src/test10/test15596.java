29e1935a01f6f72a1f1c0288b87a2a00e17c1d58
jinhuioh <aaa779979@naver.com>
2022-07-26 16:26:38 +0900

    codingTest

---
+++ codingTest10/src/test10/test15596.java
@@ -0,0 +1,21 @@
+package test10;
+
+import java.util.Scanner;
+
+//정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
+//Java: long sum(int[] a); (클래스 이름: Test)
+//a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+//리턴값: a에 포함되어 있는 정수 n개의 합
+public class test15596 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNext()) {
+			String n = scan.next();
+		}
+		//값을 담을 배열 a를 생성
+//		String[] a = ;
+	}
+
+}
