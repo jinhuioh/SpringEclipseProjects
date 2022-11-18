b63b351a61c6c16ade677f81792ea71ecf971f41
jinhuioh <aaa779979@naver.com>
2022-11-18 09:28:02 +0900

    codingTest

--- codingtest/src/codingtest/codingtest1.java
+++ codingtest/src/codingtest/codingtest1.java
@@ -2,8 +2,10 @@ package codingtest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class codingtest1 {
 
@@ -56,22 +58,63 @@ public class codingtest1 {
 		//
 		//입력
 		//첫째 줄에 A, 둘째 줄에 B, 셋째 줄에 C가 주어진다. A, B, C는 모두 100보다 크거나 같고, 1,000보다 작은 자연수이다.
-		Scanner in = new Scanner(System.in);
+//		Scanner in = new Scanner(System.in);
+//		
+//		int value = (in.nextInt() * in.nextInt() * in.nextInt());
+//		String str = Integer.toString(value);
+//		in.close();
+//		
+//		for(int i = 0; i < 10; i++) {
+//			int count = 0;
+//			for(int j = 0; j < str.length(); j++) {
+//				if((str.charAt(j)-'0')==i) {
+//					count++;
+//				}
+//			}
+//			System.out.println(count);
+//		}
 		
-		int value = (in.nextInt() * in.nextInt() * in.nextInt());
-		String str = Integer.toString(value);
-		in.close();
+//		=========================================================================
+//		나머지
+//		(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+//
+//				(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+//
+//				세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+//
+//				입력
+//				첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+//
+//				출력
+//				첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
 		
-		for(int i = 0; i < 10; i++) {
-			int count = 0;
-			for(int j = 0; j < str.length(); j++) {
-				if((str.charAt(j)-'0')==i) {
-					count++;
-				}
-			}
-			System.out.println(count);
-		}
-	
+
+//		풀이:
+//			BufferedReader 을 쓰는 방식이다.
+//			readLine() 을 통해 입력 받아 연산하는 방법 두 가지를 설명할 것이다.
+//			앞서 말했듯이 readLine() 은 한 행을 전부 읽기 때문에 공백단위로 입력해 준 문자열을 공백단위로 분리해주어야 문제를 풀 수 있을 것이다.
+//			문자열 분리 방법 두 가지로 풀어보자.
+//			1. StringTokenizer 클래스를 이용하여 분리해주는 방법
+//			2. split() 을 이용하는 방법
+//			그리고 반드시 자료형 타입을 잘 보아야 한다.
+//			st.nextToken() 은 문자열을 반환하니 Integer.parseInt()로 int 형으로 변환시켜준다.
+//			(double 형으로 풀면 나머지가 정수로 나와도 소수점까지 같이 출력되어 오답으로 처리된다.)
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//str변수에 입력받고 공백으로 문자열 분리
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		
+		//공백으로 분리한 문자를 a,b,c객체에 하나씩 할당.
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		System.out.println((a+b)%c);
+		System.out.println((a%c + b%c)%c);
+		System.out.println((a*b)%c);
+		System.out.println((a%c * b%c)%c);
 	}
 
 }
