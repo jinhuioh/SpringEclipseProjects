06c052d572ceb728e21ca76815ff3b6ef308993b
jinhuioh <aaa779979@naver.com>
2022-11-23 14:43:41 +0900

    codingTest

--- codingtest/src/codingtest/codingtest1.java
+++ codingtest/src/codingtest/codingtest1.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -216,24 +217,56 @@ public class codingtest1 {
 //		for (int i=0; i< Integer.parseInt(a); i++) {
 //			String one = st.nextToken();
 //			
-//			if(Integer.parseInt(one) == Integer.parseInt(b)) {
-//				count ++;
-////				System.out.println(count);
-//			}
+////			if(Integer.parseInt(one) == Integer.parseInt(b)) {
+////				count ++;
+//////				System.out.println(count);
+////			}
 //			
 ////			왜 string까리 비교하면 틀리는가>?
-////			if(one == b) {
-////				count ++;
-////			}
+////			스트링은 int와 다르게 참조객체라서 ==가 아니라 equals를 써야한다!!!
+//			if(one.equals(b)) {
+//				count ++;
+//			}
 //		}
 //	
 //		System.out.println(count);
+
+//		문제
+//		N개의 정수가 주어지면, 이 정수들의 합 S의 부호를 구하는 프로그램을 작성하시오.
+//
+//		입력
+//		총 3개의 테스트 셋이 주어진다. 각 테스트 셋의 첫째 줄에는 N(1 ≤ N ≤ 100,000)이 주어지고, 둘째 줄부터 N개의 줄에 걸쳐 각 정수가 주어진다. 
+//		주어지는 정수의 절댓값은 9223372036854775807보다 작거나 같다.
+//
+//		출력
+//		총 3개의 줄에 걸쳐 각 테스트 셋에 대해 N개의 정수들의 합 S의 부호를 출력한다. S=0이면 "0"을, S>0이면 "+"를, S<0이면 "-"를 출력하면 된다.
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	for(int i=0; i < 3; i++) {
+//		n을 입력받음!
+		BigInteger S = new BigInteger("0");// biginteger은 꼭 문자열 값을 넘겨주어야한다.
+		int num = Integer.parseInt(br.readLine());
+		for(int j=0; j < num; j++ ) {
+			BigInteger one = new BigInteger(br.readLine());
+			S = S.add(one);
+			
+		}//for
 		
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String name = br.readLine();
-		System.out.println(name+"??!");
+		//compareTo(0)이 -1이면 음수,1이면 양수,0이면0 출력
+		if(S.compareTo(BigInteger.ZERO)== -1) {
+			System.out.println("-");
+			
+		}
+		else if (S.compareTo(BigInteger.ZERO)==1) {
+			System.out.println("+");
+			
+		}
+		else {
+			System.out.println("0");
+			
+		}
+	}//for
+	
 	}
 
 }
