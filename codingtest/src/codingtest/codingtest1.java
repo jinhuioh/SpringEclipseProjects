4eb0bf89525261ae689edd5d06435f2f37fad93e
jinhuioh <aaa779979@naver.com>
2022-11-24 09:03:16 +0900

    codingTest

--- codingtest/src/codingtest/codingtest1.java
+++ codingtest/src/codingtest/codingtest1.java
@@ -125,18 +125,25 @@ public class codingtest1 {
 //		System.out.println(count);
 
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//		int a = Integer.parseInt(st.nextToken());
+//		int b = Integer.parseInt(st.nextToken());
+//		int c = Integer.parseInt(st.nextToken());
+//		if(b-a > c-b) {
+//			System.out.println((b-a-1));
+//		}
+//		else {
+//			System.out.println((c-b-1));
+//		}//else
 		
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		if(b-a > c-b) {
-			System.out.println((b-a-1));
-		}
-		else {
-			System.out.println((c-b-1));
-		}
+		
+		
+		
+		
+		
+		
 	}
 
 }
