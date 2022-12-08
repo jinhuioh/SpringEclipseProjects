64b6eac5f7a71e897eccc3f5c269806aa468c521
jinhuioh <aaa779979@naver.com>
2022-12-08 16:30:11 +0900

    codingTest

--- codingtest/src/codingtest/codingtest2.java
+++ codingtest/src/codingtest/codingtest2.java
@@ -1,59 +1,43 @@
 package codingtest;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.io.IOException;
+import java.io.InputStreamReader;
 //
 public class codingtest2 {
-//	//함수선언
-//	static int solve(long n) {
-//	    long sum = 0;
-//	    int addNum = 0;
-//	    while(n >= sum) {
-//	        sum += (++addNum);
-//	    }
-//
-//	    return sum == n ? addNum : addNum - 1;
-//	}
-//	
 	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		long n = Long.parseLong(br.readLine());
-//
-//	    System.out.println(solve(n));
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String nstr = br.readLine();
-		int sum = 0;
-
-//		해당 숫자 재배치 시작
-		char[] charArr = nstr.toCharArray();
-		Arrays.sort(charArr);//오름차순 정렬
-		int len = charArr.length;
-		
-		StringBuilder sb = new StringBuilder();
-		
-//		오름차순 정렬이므로, 맨 끝 원소부터 반대로 탐색
-		for(int i= len-1; i>= 0; i--) {
-			int num = charArr[i] - '0';
-//			각 자리수를 더했을 때 3의 배수이면 해당 숫자가 3의 배수이므로 sum변수에 각 자리수의 합을 담음
-			sum +=num;
-//			StringBuilder로 스트링 값 한개씩 붙임
-			sb.append(num);
+//	BABBA
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int num = Integer.parseInt(br.readLine());
+	int a0 = 1;
+	int b0 = 0;
+	
+	int a1 = 0;
+	int b1 = 1;
+	
+	int a = 0;
+	int b = 0;
+	
+	if(num ==1) {
+		System.out.println(a1+" "+b1);
+	}
+	
+	else {
+	
+		for(int i=2; i<num+1; i++) {
+			a = a0 + a1;
+			b = b0 + b1;
+	//		갱신
+			a0 = a1;
+			b0 = b1;
 			
+			a1 = a;
+			b1 = b;
 		}
+		System.out.println(a+" "+b);
 		
-//		30의 배수인지 판단하기 위한 조건 생성
-		
-		if(charArr[0] != '0' || sum % 3 != 0) {//만약 3의 배수가 아니라면
-//			-1을 출력
-			System.out.println(-1);
-			return;
-		}
-		
-		System.out.println(sb.toString());
+		}//else
+
 	}
  
 }
