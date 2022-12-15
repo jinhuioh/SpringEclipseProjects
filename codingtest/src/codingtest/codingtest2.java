8361bf80d86e38b55245c7f1b69d884adc3403e9
jinhuioh <aaa779979@naver.com>
2022-12-15 16:49:41 +0900

    codingTest

--- codingtest/src/codingtest/codingtest2.java
+++ codingtest/src/codingtest/codingtest2.java
@@ -170,34 +170,46 @@ public class codingtest2 {
 		
 		
 //	수리공 항승
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	String[] nums = br.readLine().split(" ");
-	
-	int n = Integer.parseInt(nums[0]);//고장난 파이프 개수
-	int l = Integer.parseInt(nums[1]);//테이프길이
-	
-	int answer = 0;
-//	고장난 파이프 입력받기
-	String[] pipes = br.readLine().split(" ");
-	Arrays.sort(pipes);
-//	System.out.println(pipes);
-	float pnum = 0;//테이프
-	
-	for(String p: pipes) {
-		float pint = Integer.parseInt(p);
-		// 테이프를 붙여야하면
-		if(pnum < pint + 0.5) {
-			answer +=1;
-			//pnum갱신
-			
-			pnum = (float) (pint+l-(0.5));
-//			System.out.println("pnum>> "+pnum);
-		}else {
-			continue;
-		}//else
-		
-	}//for
-	System.out.println(answer);
-	}
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());//물 새는 곳 개수
+      int l = Integer.parseInt(st.nextToken());//테이프길이
+      
+      //답이 될 변수
+      int answer = 0;
+      
+//	      고장난 파이프 입력받기
+      int[] pipes = new int[n];
+//	      토큰으로 한줄입력받아서 한개씩 끊어서 int로 형변환 후 저장
+      st = new StringTokenizer(br.readLine());
+      for(int i=0; i<pipes.length; i++) {
+         pipes[i] = Integer.parseInt(st.nextToken());
+      }
+      //sort로 작은수부터 리스트에 담기도록 한다
+      Arrays.sort(pipes);
+//	      System.out.println("pipes>> "+pipes);
+      
+      
+      float pnum = 0;//테이프
+      
+      for(int j=0; j<pipes.length; j++) {
+//	         리스트에 있는 원소 하나씩 꺼내서  pint 에 담기
+         int pint = (int) pipes[j];
+//	         System.out.println("pint"+pint);
+//	         System.out.println(pipes.length+" "+j);
+         // 리스트에서 꺼 낸 원소가 짧아서 테이프를 붙어야하는 경우
+         if(pnum < pint + 0.5) {
+            answer +=1;
+            //붙인 테이프의 마지막위치 pnum 갱신
+            pnum = (float) (pint+l-(0.5));
+//	            System.out.println("pnum>> "+pnum);
+         }else {
+            // 리스트에서 꺼 낸 원소가 길어서 테이프를 붙이지않아도 되는 경우
+            continue;
+         }//else
+      }//for
+      System.out.println(answer);
+   }
+
 }
+	
