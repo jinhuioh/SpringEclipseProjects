8c71919293bb0d8a77ecad1222690652400cb972
jinhuioh <aaa779979@naver.com>
2023-05-10 21:32:09 +0900

    codingTest

--- codingtest/src/codingtest/Main.java
+++ codingtest/src/codingtest/Main.java
@@ -1,6 +1,5 @@
 package codingtest;
 
-
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
@@ -18,59 +17,77 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 import javax.management.Query;
 import javax.swing.JPopupMenu.Separator;
-//차이를 최대로
+//문제
+//DNA란 어떤 유전물질을 구성하는 분자이다. 이 DNA는 서로 다른 4가지의 뉴클레오티드로 이루어져 있다(Adenine, Thymine, Guanine, Cytosine). 
+//우리는 어떤 DNA의 물질을 표현할 때, 이 DNA를 이루는 뉴클레오티드의 첫글자를 따서 표현한다. 만약에 Thymine-Adenine-Adenine-Cytosine-Thymine-Guanine-Cytosine-Cytosine-Guanine-Adenine-Thymine로 이루어진 DNA가 있다고 하면, “TAACTGCCGAT”로 표현할 수 있다. 
+//그리고 Hamming Distance란 길이가 같은 두 DNA가 있을 때, 각 위치의 뉴클오티드 문자가 다른 것의 개수이다. 만약에 “AGCAT"와 ”GGAAT"는 첫 번째 글자와 세 번째 글자가 다르므로 Hamming Distance는 2이다.
+//
+//우리가 할 일은 다음과 같다. N개의 길이 M인 DNA s1, s2, ..., sn가 주어져 있을 때 Hamming Distance의 합이 가장 작은 DNA s를 구하는 것이다. 즉, s와 s1의 Hamming Distance + s와 s2의 Hamming Distance + s와 s3의 Hamming Distance ... 의 합이 최소가 된다는 의미이다.
+//
 //입력
-//첫째 줄에 N (3 ≤ N ≤ 8)이 주어진다. 둘째 줄에는 배열 A에 들어있는 정수가 주어진다. 배열에 들어있는 정수는 -100보다 크거나 같고, 100보다 작거나 같다.
+//첫 줄에 DNA의 수 N과 문자열의 길이 M이 주어진다. 그리고 둘째 줄부터 N+1번째 줄까지 N개의 DNA가 주어진다. N은 1,000보다 작거나 같은 자연수이고, M은 50보다 작거나 같은 자연수이다.
 //
 //출력
-//첫째 줄에 배열에 들어있는 수의 순서를 적절히 바꿔서 얻을 수 있는 식의 최댓값을 출력한다.
-
+//첫째 줄에 Hamming Distance의 합이 가장 작은 DNA 를 출력하고, 둘째 줄에는 그 Hamming Distance의 합을 출력하시오. 그러한 DNA가 여러 개 있을 때에는 사전순으로 가장 앞서는 것을 출력한다.
+//
 //예제 입력 1 
-//6
-//20 1 15 8 4 10
+//5 8
+//TATGATAC
+//TAAGCTAC
+//AAAGATCC
+//TGAGATAC
+//TAAGATGT
 //예제 출력 1 
-//62
+//TAAGATAC
+//7
 
 public class Main {
-	static boolean[] visited;
-	static int[] arr, change_arr;
-	static int n, max_sum, sum;//숫자의 개수 
-	//만들함수: 1. sum() : change_arr 순서대로 연산하며 합한 값 구하는 함수
-	//		 2. dfs(int x) : change_arr 을 만드는 함수. 즉 arr에 들어있는 숫자 순서 바꾸는 dfs
-	
-	private static int sum() {
-		int abs_sum = 0;
-		for(int i = 1; i<n; i++) {
-			abs_sum += Math.abs(change_arr[i] - change_arr[i-1]);
-		}
-		
-		return abs_sum;
-	}
-	private static void dfs(int x) {
-		if(x == n) {
-			max_sum = Math.max(max_sum, sum());
-		}
-		for(int i = 0; i<n; i++) {
-			if(!visited[i]) {
-				change_arr[x] = arr[i];
-				visited[i] = true;
-				dfs(x+1);
-				visited[i] = false;
-			}
-		}
-	}
-	
 	public static void main(String[] args)throws Exception {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		arr = new int[n];
-		change_arr = new int[n];
-		visited = new boolean[n];
-		for(int i = 0; i<n; i++) {
-			arr[i] = sc.nextInt();
-		}//for
-		
-		dfs(0);
-		System.out.println(max_sum);
+
 	}
 }
+
+
+//public class Main {
+//	static boolean[] visited;
+//	static int[] arr, change_arr;
+//	static int n, max_sum, sum;//숫자의 개수 
+//	//만들함수: 1. sum() : change_arr 순서대로 연산하며 합한 값 구하는 함수
+//	//		 2. dfs(int x) : change_arr 을 만드는 함수. 즉 arr에 들어있는 숫자 순서 바꾸는 dfs
+//	
+//	private static int sum() {
+//		int abs_sum = 0;
+//		for(int i = 1; i<n; i++) {
+//			abs_sum += Math.abs(change_arr[i] - change_arr[i-1]);
+//		}
+//		
+//		return abs_sum;
+//	}
+//	private static void dfs(int x) {
+//		if(x == n) {
+//			max_sum = Math.max(max_sum, sum());
+//		}
+//		for(int i = 0; i<n; i++) {
+//			if(!visited[i]) {
+//				change_arr[x] = arr[i];
+//				visited[i] = true;
+//				dfs(x+1);
+//				visited[i] = false;
+//			}
+//		}
+//	}
+//	
+//	public static void main(String[] args)throws Exception {
+//		Scanner sc = new Scanner(System.in);
+//		n = sc.nextInt();
+//		arr = new int[n];
+//		change_arr = new int[n];
+//		visited = new boolean[n];
+//		for(int i = 0; i<n; i++) {
+//			arr[i] = sc.nextInt();
+//		}//for
+//		
+//		dfs(0);
+//		System.out.println(max_sum);
+//	}
+//}
--- codingtest/src/codingtest/codingTest3.java
+++ codingtest/src/codingtest/codingTest3.java
@@ -5,104 +5,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
 import javax.swing.plaf.SliderUI;
-//	문제
-//	N×M 크기의 공간에 아기 상어 여러 마리가 있다. 공간은 1×1 크기의 정사각형 칸으로 나누어져 있다. 한 칸에는 아기 상어가 최대 1마리 존재한다.
+
+//문제
+//N이 주어졌을 때, 1부터 N까지의 수로 이루어진 순열을 사전순으로 출력하는 프로그램을 작성하시오.
 //
-//	어떤 칸의 안전 거리는 그 칸과 가장 거리가 가까운 아기 상어와의 거리이다. 두 칸의 거리는 하나의 칸에서 다른 칸으로 가기 위해서 지나야 하는 칸의 수이고, 이동은 인접한 8방향(대각선 포함)이 가능하다.
+//입력
+//첫째 줄에 N(1 ≤ N ≤ 8)이 주어진다. 
 //
-//	안전 거리가 가장 큰 칸을 구해보자. 
+//출력
+//첫째 줄부터 N!개의 줄에 걸쳐서 모든 순열을 사전순으로 출력한다.
 //
-//	입력
-//	첫째 줄에 공간의 크기 N과 M(2 ≤ N, M ≤ 50)이 주어진다. 둘째 줄부터 N개의 줄에 공간의 상태가 주어지며, 0은 빈 칸, 1은 아기 상어가 있는 칸이다. 빈 칸과 상어의 수가 각각 한 개 이상인 입력만 주어진다.
-//
-//	출력
-//	첫째 줄에 안전 거리의 최댓값을 출력한다.
-//
-//	예제 입력 1 
-//	5 4
-//	0 0 1 0
-//	0 0 0 0
-//	1 0 0 0
-//	0 0 0 0
-//	0 0 0 1
-//	예제 출력 1 
-//	2	
-class node{
-	int y;
-	int x;
-	node(int y, int x){
-		this.y = y;
-		this.x = x;
-	}
-}
+//예제 입력 1 
+//3
+//예제 출력 1 
+//1 2 3
+//1 3 2
+//2 1 3
+//2 3 1
+//3 1 2
+//3 2 1
 
 public class codingTest3 {
-	static int[] dy = {0,0,-1,-1,1,1,1,-1};
-	static int[] dx = {1,-1,1,-1,1,-1,0,0};
-	static int[][] map;
-	static int[][] arr;
-	static boolean[][] visited;
-	static int n,m;
-	
-	private static int bfs(int i, int j) {
-		visited = new boolean[n][m];
-		arr = new int[n][m];
-		Queue<node> q = new LinkedList<node>();
-		int count = Integer.MAX_VALUE;//안전거리 계산 변수
-		
-		q.add(new node(i, j));
-		visited[i][j] = true;
-		
-		while (!q.isEmpty()) {
-			node qp = q.poll();
-			for(int k = 0; k<8; k++) {
-				int ny = qp.y + dy[k];
-				int nx = qp.x + dx[k];
-				if(ny<0 || nx<0 || ny>=n || nx>=m || visited[ny][nx]) continue;
+	static int n;
+	static int[] arr, change_arr;
+	static boolean[] visited;
+	private static void dfs(int index) {
+		if(index == n) {
+			for(int i = 0; i < n; i++) {
+				System.out.print(change_arr[i]+" ");
+			}
+			System.out.println();
+		}//if
+		for(int i = 0; i < n; i++) {
+			if(!visited[i]) {
 				
-				if(map[ny][nx] == 1) {
-					count = Math.min(count, arr[qp.y][qp.x] + 1);
-//					System.out.println(qp.y+" "+qp.x+"count!!>>"+ count);
-					break;
-				}
-				arr[ny][nx] = arr[qp.y][qp.x] + 1;
-//				System.out.println("qp.y>>"+qp.y + " qp.x>> "+qp.x +" nynx>>"+ ny+" "+nx+" "+" arrnynx>>" + arr[ny][nx]);
-				visited[ny][nx] = true;
-				q.add(new node(ny, nx));
+				visited[i] = true;
+				change_arr[index] = arr[i];
+				dfs(index+1);
+				visited[i] = false;
+				
 			}
 		}
-		return count;
 	}
 	
-	public static void main(String[] args) throws NumberFormatException, IOException, InterruptedException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		visited = new boolean[n];
+		arr = new int[n];
+		change_arr = new int[n];
 		
-		map = new int[n][m];
-
-		//map입력받기
-		for(int i = 0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j<m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}//for
-		}//for
+		for(int i = 0; i < n; i++) {
+			arr[i] = i+1; 
+		}
+		dfs(0);
 		
-		//첫번째 위치부터 가장 가까운 아기상어와의 거리 구하기
-		int answer = 1;//초기값
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<m; j++) {
-				if( map[i][j] != 1) {
-					int answer_bfs = bfs(i,j); //해당 위치의 안전거리 값
-					answer = Math.max(answer, answer_bfs);
-				}//if
-			}//for
-		}//for
-		System.out.println(answer);
 	}
-
 }
