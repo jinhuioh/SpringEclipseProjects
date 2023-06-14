0c4dfac787f9e4cd16040fa9df5296f0c72c90cd
jinhuioh <aaa779979@naver.com>
2023-06-14 18:56:27 +0900

    codingTest

--- codingtest/src/codingtest/Main.java
+++ codingtest/src/codingtest/Main.java
@@ -18,56 +18,46 @@ import java.util.StringTokenizer;
 import javax.management.Query;
 import javax.swing.JPopupMenu.Separator;
 
+//문제
+//길이가 N으로 같은 문자열 X와 Y가 있을 때, 두 문자열 X와 Y의 차이는 X[i] ≠ Y[i]인 i의 개수이다. 예를 들어, X=”jimin”, Y=”minji”이면, 둘의 차이는 4이다.
+//
+//두 문자열 A와 B가 주어진다. 이때, A의 길이는 B의 길이보다 작거나 같다. 이제 A의 길이가 B의 길이와 같아질 때 까지 다음과 같은 연산을 할 수 있다.
+//
+//A의 앞에 아무 알파벳이나 추가한다.
+//A의 뒤에 아무 알파벳이나 추가한다.
+//이때, A와 B의 길이가 같으면서, A와 B의 차이를 최소로 하는 프로그램을 작성하시오.
+//
 //입력
-//첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스의 첫째 줄에는 n (1 ≤ n ≤ 100,000)이 주어진다. 
-//다음 두 줄에는 n개의 정수가 주어지며, 각 정수는 그 위치에 해당하는 스티커의 점수이다. 연속하는 두 정수 사이에는 빈 칸이 하나 있다. 
-//점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다. 
+//첫째 줄에 A와 B가 주어진다. A와 B의 길이는 최대 50이고, A의 길이는 B의 길이보다 작거나 같고, 알파벳 소문자로만 이루어져 있다.
 //
 //출력
-//각 테스트 케이스 마다, 2n개의 스티커 중에서 두 변을 공유하지 않는 스티커 점수의 최댓값을 출력한다.
+//A와 B의 길이가 같으면서, A와 B의 차이를 최소가 되도록 했을 때, 그 차이를 출력하시오.
 //
 //예제 입력 1 
-//2
-//5
-//50 10 100 20 40
-//30 50 70 10 60
-//7
-//10 30 10 50 100 20 40
-//20 40 30 50 60 20 80
+//adaabc aababbc
 //예제 출력 1 
-//260
-//290
+//2
 
+//시간복잡도 O(n^2)
 public class Main {
 	public static void main(String[] args)throws Exception {
+
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();//테스트케이스 개수
-		for(int i = 0; i < n; i++) {
-			int d = sc.nextInt();//스티커 개수
-			int[][] dp = new int[2][d+1];
-			
-			for(int k = 0; k<2; k++) {
-				for(int j = 1; j<d+1; j++) {
-					dp[k][j] = sc.nextInt();
-				}
+		String A = sc.next();
+		String B = sc.next();
+		int answer = Integer.MAX_VALUE;
+		for(int i = 0; i<B.length()- A.length()+1; i++) {
+			int count = 0;
+			for(int j = 0; j<A.length(); j++) {
+				if(A.charAt(j) != B.charAt(i+j)) {
+					count++;
+				}//if
 			}//for
-			for(int k = 2; k<d+1; k++) {
-					dp[0][k] = Math.max(dp[1][k-2], dp[1][k-1])+dp[0][k];
-					dp[1][k] = Math.max(dp[0][k-2], dp[0][k-1])+ dp[1][k];
-					
-//					for(int p = 0; p<2; p++) {
-//						for(int l = 0; l<d; l++) {
-//							System.out.print(dp[p][l]+" ");
-//						}
-//						System.out.println();
-//					}
-//					System.out.println();
-			}//for
-			int max_num = Math.max(dp[0][d], dp[1][d]);
-			System.out.println(max_num);
+			if(answer > count) {
+				answer = count;
+			}
 		}//for
-		
-		
+		System.out.println(answer);
 	}
 }
 
